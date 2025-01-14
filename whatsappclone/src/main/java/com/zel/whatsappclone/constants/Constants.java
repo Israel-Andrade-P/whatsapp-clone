3f664b825df278e1d428c3d6a716f9d557b28101
Israel-Andrade-P <israel88andrade@gmail.com>
2025-01-14 11:36:28 -0300

    add security config

---
+++ whatsappclone/src/main/java/com/zel/whatsappclone/constants/Constants.java
@@ -0,0 +1,18 @@
+package com.zel.whatsappclone.constants;
+
+public class Constants {
+    public static final String[] WHITE_LIST = {
+            "/auth/**",
+            "/v2/api-docs",
+            "/v3/api-docs",
+            "/v3/api-docs/**",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui/**",
+            "/webjars/**",
+            "/swagger-ui.html",
+            "/ws/**"
+    };
+}
---
+++ whatsappclone/src/main/java/com/zel/whatsappclone/security/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.zel.whatsappclone.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static com.zel.whatsappclone.constants.Constants.WHITE_LIST;
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(req ->
+                        req.requestMatchers(WHITE_LIST).permitAll()
+                                .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(auth ->
+                        auth.jwt(token ->
+                                token.jwtAuthenticationConverter(new KeycloakJwtAuthenticationConverter())));
+
+        return http.build();
+    }
+}
