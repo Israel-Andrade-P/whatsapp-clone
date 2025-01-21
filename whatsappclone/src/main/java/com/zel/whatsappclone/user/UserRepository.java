bd6254599bb279ba477ba22662b22851983c155b
Israel-Andrade-P <israel88andrade@gmail.com>
2025-01-21 11:24:41 -0300

    add user service and controller, and ws config

--- whatsappclone/src/main/java/com/zel/whatsappclone/user/UserRepository.java
+++ whatsappclone/src/main/java/com/zel/whatsappclone/user/UserRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 import static com.zel.whatsappclone.constants.Constants.*;
@@ -15,4 +16,6 @@ public interface UserRepository extends JpaRepository<User, String> {
 
     @Query(name = FIND_USER_BY_PUBLIC_ID)
     Optional<User> findByPublicId(@Param("publicId") String publicId);
+    @Query(name = FIND_ALL_USERS_EXCEPT_SELF)
+    List<User> findAllUsersExceptSelf(@Param("publicId") String publicId);
 }
--- whatsappclone/src/main/java/com/zel/whatsappclone/user/UserService.java
+++ whatsappclone/src/main/java/com/zel/whatsappclone/user/UserService.java
@@ -1,10 +1,18 @@
 package com.zel.whatsappclone.user;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
-    
+    private final UserRepository userRepository;
+    private final UserMapper mapper;
+
+    public List<UserResponse> getAllUsersExceptSelf(Authentication loggedUser){
+        return userRepository.findAllUsersExceptSelf(loggedUser.getName()).stream().map(mapper::fromUser).toList();
+    }
 }
