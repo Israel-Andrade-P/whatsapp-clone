dab369b8812fd82b01affffc7e2619ff97671afd
Israel <israel88andrade@gmail.com>
2025-01-30 17:41:59 -0300

    add contact ui

--- whatsapp-clone-ui/src/app/components/chat-list/chat-list.component.ts
+++ whatsapp-clone-ui/src/app/components/chat-list/chat-list.component.ts
@@ -1,9 +1,11 @@
 import { Component, input, InputSignal } from '@angular/core';
-import { ChatResponse } from '../../services/models';
+import { ChatResponse, UserResponse } from '../../services/models';
+import { DatePipe } from '@angular/common';
+import { UserService } from '../../services/services';
 
 @Component({
   selector: 'app-chat-list',
-  imports: [],
+  imports: [ DatePipe ],
   templateUrl: './chat-list.component.html',
   styleUrl: './chat-list.component.scss'
 })
@@ -11,12 +13,34 @@ export class ChatListComponent {
 
   chats: InputSignal<ChatResponse[]> = input<ChatResponse[]>([]);
   searchNewContact: boolean = false;
+  contacts: Array<UserResponse> = [];
+
+  constructor(private userService: UserService) {
+
+  }
 
   searchContact(){
-
+    this.userService.getAllUsers()
+    .subscribe({
+      next: (users) => {
+        this.contacts = users;
+        this.searchNewContact = true
+      }
+    })
   }
 
   chatClicked(chat: ChatResponse){
 
+  }
+
+  wrapMessage(lastMessage: string | undefined): string{
+    if (lastMessage && lastMessage.length <= 20){
+      return lastMessage;
+    }
+    return lastMessage?.substring(0, 17) + '...';
+  }
+
+  selectContact(contact: UserResponse){
+    
   }
 }
